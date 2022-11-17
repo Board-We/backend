736b783818bcc876ef5ef0f0208a13df2e9514a3
jungyeunwon <jungyeun_won@tmax.co.kr>
2022-11-17 13:50:53 +0900

    [feat] Initial Commit

---
+++ src/main/java/com/boardwe/boardwe/BoardWeApplication.java
@@ -0,0 +1,13 @@
+package com.boardwe.boardwe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardWeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardWeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boardwe/boardwe/BoardWeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardwe.boardwe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardWeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
