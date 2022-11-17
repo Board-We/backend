fd1ea0e7a9d53d3b1000521918817011b32eaf4a
hyunwoo_kim3 <hyunwoo_kim3@tmax.co.kr>
2022-11-17 20:51:39 +0900

    add Board and Tag Entity class code

--- src/main/java/com/boardwe/boardwe/entity/Board.java
+++ src/main/java/com/boardwe/boardwe/entity/Board.java
@@ -1,4 +1,73 @@
 package com.boardwe.boardwe.entity;
 
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "BOARD")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Board {
+    @Id
+    @Column(name = "board_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long boardId;
+
+    @ManyToOne
+    @JoinColumn(name = "board_theme_id", referencedColumnName = "board_theme_id")
+    @NotNull
+    private long boardThemeId;
+
+    @Column(name = "board_name", length = 20)
+    @NotNull
+    private String boardName;
+
+    @Column(name = "board_description", length = 50)
+    private String boardDescription;
+
+    @Column(name = "board_code", length = 36)
+    @NotNull
+    private String boardCode;
+
+    @Column(name = "board_writing_start_time")
+    @NotNull
+    private Timestamp boardWritingStartTime;
+
+    @Column(name = "board_writing_end_time")
+    @NotNull
+    private Timestamp boardWritingEndTime;
+
+    @Column(name = "board_open_start_time")
+    @NotNull
+    private Timestamp boardOpenStartTime;
+
+    @Column(name = "board_open_end_time")
+    @NotNull
+    private Timestamp boardOpenEndTime;
+
+    @Column(name = "board_password", length = 16)
+    @NotNull
+    private String boardPassword;
+    
+    @Column(name = "board_open_type", length = 10)
+    @NotNull
+    private String boardOpenType;
+
+    @Column(name = "board_views")
+    @NotNull
+    private String boardViews;
 }
--- src/main/java/com/boardwe/boardwe/entity/Tag.java
+++ src/main/java/com/boardwe/boardwe/entity/Tag.java
@@ -1,4 +1,36 @@
 package com.boardwe.boardwe.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "TAG")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Tag {
+    @Id
+    @Column(name = "tag_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long tagId;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id", referencedColumnName = "board_id")
+    @NotNull
+    private long boardId;
+
+    @Column(name = "tag_value", length = 20)
+    @NotNull
+    private String tagValue;
 }
