735dfc148eedbf63360cf97f47d3dbe2485f6fd9
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-11-17 20:53:37 +0900

    [feat] 보드 테마, 테마 카테고리 엔티티 구현

--- src/main/java/com/boardwe/boardwe/entity/BoardTheme.java
+++ src/main/java/com/boardwe/boardwe/entity/BoardTheme.java
@@ -1,4 +1,50 @@
 package com.boardwe.boardwe.entity;
 
+import com.boardwe.boardwe.type.BackgroundType;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BoardTheme {
+
+    @Id @GeneratedValue
+    @Column(name="board_theme_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "theme_category_id")
+    private ThemeCategory themeCategory;
+
+    @Column(columnDefinition = "NVARCHAR2(20)",name = "board_theme_name")
+    private String name;
+
+    @Column(columnDefinition = "NVARCHAR2(10)",name="board_theme_background_type")
+    private BackgroundType backgroundType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="board_theme_background_image_id")
+    private ImageInfo backgroundImageInfo;
+
+
+    @Column(columnDefinition = "NVARCHAR2(7)",name="board_theme_background_color")
+    private String backgroundColor;
+
+    @Column(columnDefinition = "NVARCHAR2(20)",name="board_theme_font")
+    private String font;
+
+    @Builder
+    public BoardTheme(ThemeCategory themeCategory, String name, BackgroundType backgroundType, ImageInfo backgroundImageInfo, String backgroundColor, String font) {
+        this.themeCategory = themeCategory;
+        this.name = name;
+        this.backgroundType = backgroundType;
+        this.backgroundImageInfo = backgroundImageInfo;
+        this.backgroundColor = backgroundColor;
+        this.font = font;
+    }
 }
--- src/main/java/com/boardwe/boardwe/entity/ThemeCategory.java
+++ src/main/java/com/boardwe/boardwe/entity/ThemeCategory.java
@@ -1,4 +1,28 @@
 package com.boardwe.boardwe.entity;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ThemeCategory {
+    @Id @GeneratedValue
+    @Column(name="theme_category_id")
+    private Long id;
+
+    @Column(columnDefinition = "NVARCHAR2(50)",name="theme_category_name")
+    private String name;
+
+    @Builder
+    public ThemeCategory(String name) {
+        this.name = name;
+    }
 }
