6dfc7efbcb863f2e6f6aea59ceebfac7b3185ada
jjungyeun <wjyddd@naver.com>
2022-11-20 15:44:44 +0900

    [feat] BoardStatus에 보드 상태를 계산하는 메소드 추가

--- src/main/java/com/boardwe/boardwe/type/BoardStatus.java
+++ src/main/java/com/boardwe/boardwe/type/BoardStatus.java
@@ -1,5 +1,24 @@
 package com.boardwe.boardwe.type;
 
+import java.time.LocalDateTime;
+
 public enum BoardStatus {
     BEFORE_WRITING, WRITING, BEFORE_OPEN, OPEN, CLOSED;
+
+    public static BoardStatus calculateBoardStatus(
+            LocalDateTime writingStartTime, LocalDateTime writingEndTime,
+            LocalDateTime openStartTime, LocalDateTime openEndTime) {
+        LocalDateTime currentTime = LocalDateTime.now();
+        if (currentTime.isBefore(writingStartTime)) {
+            return BEFORE_WRITING;
+        } else if (currentTime.isBefore(writingEndTime)) {
+            return WRITING;
+        } else if (currentTime.isBefore(openStartTime)) {
+            return BEFORE_OPEN;
+        } else if (currentTime.isBefore(openEndTime)) {
+            return OPEN;
+        } else {
+            return CLOSED;
+        }
+    }
 }
