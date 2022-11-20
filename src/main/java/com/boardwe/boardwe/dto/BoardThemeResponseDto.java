2fe774b681792b2958d56804e3adba8e691e8c77
jjungyeun <wjyddd@naver.com>
2022-11-20 18:33:49 +0900

    [feat] 테마 조회 응답을 위한 DTO 클래스 정의
    - 테마 조회 응답을 위한 BoardThemeResponseDto 및 MemoThemeResponseDto 필드 추가

--- src/main/java/com/boardwe/boardwe/dto/BoardThemeResponseDto.java
+++ src/main/java/com/boardwe/boardwe/dto/BoardThemeResponseDto.java
@@ -1,4 +1,18 @@
 package com.boardwe.boardwe.dto;
 
+import com.boardwe.boardwe.type.BackgroundType;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class BoardThemeResponseDto {
+    private BackgroundType boardBackgroundType;
+    private String boardBackground;
+    private String boardFont;
+    private List<MemoThemeResponseDto> memoThemes;
 }
