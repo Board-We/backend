79e6d65133912d237751fa0af472d46f31a990a8
jjungyeun <wjyddd@naver.com>
2022-11-20 15:46:36 +0900

    [feat] 보드 조회 메소드 구현 2
    - 작성 상태의 PUBLIC 보드를 조회하기 위한 로직 구현 및 테스트 작성
    - 작성 이전, 공개 이전, 공개 종료 상태의 PUBLIC 보드를 조회 실패하는 로직 구현 및 테스트 작성

--- src/main/java/com/boardwe/boardwe/service/impl/BoardSelectServiceImpl.java
+++ src/main/java/com/boardwe/boardwe/service/impl/BoardSelectServiceImpl.java
@@ -2,6 +2,9 @@ package com.boardwe.boardwe.service.impl;
 
 import com.boardwe.boardwe.dto.BoardSelectResponseDto;
 import com.boardwe.boardwe.entity.Board;
+import com.boardwe.boardwe.exception.custom.BoardBeforeOpenException;
+import com.boardwe.boardwe.exception.custom.BoardBeforeWritingException;
+import com.boardwe.boardwe.exception.custom.BoardClosedException;
 import com.boardwe.boardwe.exception.custom.BoardNotFoundException;
 import com.boardwe.boardwe.repository.BoardRepository;
 import com.boardwe.boardwe.service.BoardSelectService;
@@ -19,13 +22,32 @@ public class BoardSelectServiceImpl implements BoardSelectService {
     public BoardSelectResponseDto getBoard(String boardCode) {
         Board board = boardRepository.findByCode(boardCode)
                 .orElseThrow(BoardNotFoundException::new);
+
+        BoardStatus boardStatus = BoardStatus.calculateBoardStatus(
+                board.getWritingStartTime(),
+                board.getWritingEndTime(),
+                board.getOpenStartTime(),
+                board.getOpenEndTime());
+
+        validateBoardStatus(boardStatus);
+
         return BoardSelectResponseDto.builder()
                 .boardName(board.getName())
                 .boardDescription(board.getDescription())
                 .openStartTime(board.getOpenStartTime())
                 .openEndTime(board.getOpenEndTime())
                 .openType(board.getOpenType())
-                .boardStatus(BoardStatus.OPEN)
+                .boardStatus(boardStatus)
                 .build();
+    }
+
+    private static void validateBoardStatus(BoardStatus boardStatus) {
+        if (boardStatus == BoardStatus.BEFORE_WRITING) {
+            throw new BoardBeforeWritingException();
+        } else if (boardStatus == BoardStatus.BEFORE_OPEN) {
+            throw new BoardBeforeOpenException();
+        } else if (boardStatus == BoardStatus.CLOSED) {
+            throw new BoardClosedException();
+        }
     }
 }
--- src/test/java/com/boardwe/boardwe/service/impl/BoardSelectServiceImplTest.java
+++ src/test/java/com/boardwe/boardwe/service/impl/BoardSelectServiceImplTest.java
@@ -2,8 +2,10 @@ package com.boardwe.boardwe.service.impl;
 
 import com.boardwe.boardwe.dto.BoardSelectResponseDto;
 import com.boardwe.boardwe.entity.Board;
+import com.boardwe.boardwe.exception.custom.BoardBeforeOpenException;
+import com.boardwe.boardwe.exception.custom.BoardBeforeWritingException;
+import com.boardwe.boardwe.exception.custom.BoardClosedException;
 import com.boardwe.boardwe.repository.BoardRepository;
-import com.boardwe.boardwe.service.BoardSelectService;
 import com.boardwe.boardwe.type.BoardStatus;
 import com.boardwe.boardwe.type.OpenType;
 import org.junit.jupiter.api.DisplayName;
@@ -16,8 +18,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class BoardSelectServiceImplTest {
@@ -36,11 +40,15 @@ class BoardSelectServiceImplTest {
         Board board = mock(Board.class);
         String boardName = "내 생일을 축하해줘";
         String boardDesc = "생일 축하하는 메시지를 써줘";
-        LocalDateTime openStartTime = LocalDateTime.of(2022, 9, 15, 0, 0);
-        LocalDateTime openEndTime = LocalDateTime.of(2022, 9, 18, 23, 59, 59);
+        LocalDateTime writingStartTime = LocalDateTime.now().minusDays(3);
+        LocalDateTime writingEndTime = writingStartTime.plusDays(1);
+        LocalDateTime openStartTime = LocalDateTime.now().minusDays(1);
+        LocalDateTime openEndTime = openStartTime.plusDays(2);
 
         when(board.getName()).thenReturn(boardName);
         when(board.getDescription()).thenReturn(boardDesc);
+        when(board.getWritingStartTime()).thenReturn(writingStartTime);
+        when(board.getWritingEndTime()).thenReturn(writingEndTime);
         when(board.getOpenStartTime()).thenReturn(openStartTime);
         when(board.getOpenEndTime()).thenReturn(openEndTime);
         when(board.getOpenType()).thenReturn(OpenType.PUBLIC);
@@ -58,6 +66,114 @@ class BoardSelectServiceImplTest {
         assertEquals(openEndTime, responseDto.getOpenEndTime());
         assertEquals(OpenType.PUBLIC, responseDto.getOpenType());
         assertEquals(BoardStatus.OPEN, responseDto.getBoardStatus());
+    }
+
+    @Test
+    @DisplayName("작성 상태의 PUBLIC 보드를 조회한다.")
+    void get_writing_board_success() throws Exception {
+        // given
+        String boardCode = "1234";
+        Board board = mock(Board.class);
+        String boardName = "내 생일을 축하해줘";
+        String boardDesc = "생일 축하하는 메시지를 써줘";
+        LocalDateTime writingStartTime = LocalDateTime.now().minusDays(1);
+        LocalDateTime writingEndTime = writingStartTime.plusDays(2);
+        LocalDateTime openStartTime = writingEndTime.plusDays(3);
+        LocalDateTime openEndTime = openStartTime.plusDays(1);
+
+        when(board.getName()).thenReturn(boardName);
+        when(board.getDescription()).thenReturn(boardDesc);
+        when(board.getWritingStartTime()).thenReturn(writingStartTime);
+        when(board.getWritingEndTime()).thenReturn(writingEndTime);
+        when(board.getOpenStartTime()).thenReturn(openStartTime);
+        when(board.getOpenEndTime()).thenReturn(openEndTime);
+        when(board.getOpenType()).thenReturn(OpenType.PUBLIC);
+
+        when(boardRepository.findByCode(boardCode))
+                .thenReturn(Optional.of(board));
+
+        // when
+        BoardSelectResponseDto responseDto = boardSelectService.getBoard(boardCode);
+
+        // then
+        assertEquals(boardName, responseDto.getBoardName());
+        assertEquals(boardDesc, responseDto.getBoardDescription());
+        assertEquals(openStartTime, responseDto.getOpenStartTime());
+        assertEquals(openEndTime, responseDto.getOpenEndTime());
+        assertEquals(OpenType.PUBLIC, responseDto.getOpenType());
+        assertEquals(BoardStatus.WRITING, responseDto.getBoardStatus());
+    }
+
+    @Test
+    @DisplayName("작성 이전 상태의 PUBLIC 보드를 조회하는데 실패한다.")
+    void get_before_writing_board_success() throws Exception {
+        // given
+        String boardCode = "1234";
+        Board board = mock(Board.class);
+        LocalDateTime writingStartTime = LocalDateTime.now().plusDays(1);
+        LocalDateTime writingEndTime = writingStartTime.plusDays(2);
+        LocalDateTime openStartTime = writingEndTime.plusDays(1);
+        LocalDateTime openEndTime = openStartTime.plusDays(1);
+
+        when(board.getWritingStartTime()).thenReturn(writingStartTime);
+        when(board.getWritingEndTime()).thenReturn(writingEndTime);
+        when(board.getOpenStartTime()).thenReturn(openStartTime);
+        when(board.getOpenEndTime()).thenReturn(openEndTime);
+//        when(board.getOpenType()).thenReturn(OpenType.PUBLIC);
+
+        when(boardRepository.findByCode(boardCode))
+                .thenReturn(Optional.of(board));
+
+        // when & then
+        assertThrows(BoardBeforeWritingException.class, () -> boardSelectService.getBoard(boardCode));
+    }
+
+    @Test
+    @DisplayName("공개 이전 상태의 PUBLIC 보드를 조회하는데 실패한다.")
+    void get_before_open_board_success() throws Exception {
+        // given
+        String boardCode = "1234";
+        Board board = mock(Board.class);
+        LocalDateTime writingStartTime = LocalDateTime.now().minusDays(5);
+        LocalDateTime writingEndTime = writingStartTime.plusDays(2);
+        LocalDateTime openStartTime = LocalDateTime.now().plusDays(1);
+        LocalDateTime openEndTime = openStartTime.plusDays(1);
+
+        when(board.getWritingStartTime()).thenReturn(writingStartTime);
+        when(board.getWritingEndTime()).thenReturn(writingEndTime);
+        when(board.getOpenStartTime()).thenReturn(openStartTime);
+        when(board.getOpenEndTime()).thenReturn(openEndTime);
+//        when(board.getOpenType()).thenReturn(OpenType.PUBLIC);
+
+        when(boardRepository.findByCode(boardCode))
+                .thenReturn(Optional.of(board));
+
+        // when & then
+        assertThrows(BoardBeforeOpenException.class, () -> boardSelectService.getBoard(boardCode));
+    }
+
+    @Test
+    @DisplayName("공개 종료 상태의 PUBLIC 보드를 조회하는데 실패한다.")
+    void get_closed_board_success() throws Exception {
+        // given
+        String boardCode = "1234";
+        Board board = mock(Board.class);
+        LocalDateTime writingStartTime = LocalDateTime.now().minusDays(10);
+        LocalDateTime writingEndTime = writingStartTime.plusDays(2);
+        LocalDateTime openStartTime = writingEndTime.plusDays(1);
+        LocalDateTime openEndTime = openStartTime.plusDays(1);
+
+        when(board.getWritingStartTime()).thenReturn(writingStartTime);
+        when(board.getWritingEndTime()).thenReturn(writingEndTime);
+        when(board.getOpenStartTime()).thenReturn(openStartTime);
+        when(board.getOpenEndTime()).thenReturn(openEndTime);
+//        when(board.getOpenType()).thenReturn(OpenType.PUBLIC);
+
+        when(boardRepository.findByCode(boardCode))
+                .thenReturn(Optional.of(board));
+
+        // when & then
+        assertThrows(BoardClosedException.class, () -> boardSelectService.getBoard(boardCode));
     }
 
 
