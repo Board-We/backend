673251504497f0dcf762f364f41ced69e248f400
jjungyeun <wjyddd@naver.com>
2022-12-07 16:36:05 +0900

    [bugfix] API 스펙에 맞지 않는 요청이 들어오는 경우 400 오류가 발생하도록 수정
    - 기존에는 500 에러로 나갔는데, MissingServletRequestParameterException에 대한 처리 로직을 추가하여 400으로 나가도록 수정

--- src/main/java/com/boardwe/boardwe/exception/ControllerExceptionHandler.java
+++ src/main/java/com/boardwe/boardwe/exception/ControllerExceptionHandler.java
@@ -2,6 +2,9 @@ package com.boardwe.boardwe.exception;
 
 import com.boardwe.boardwe.exception.custom.CustomException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,6 +17,22 @@ public class ControllerExceptionHandler {
         ErrorResponseDto response = ErrorResponseDto.error(errorCode);
         return ResponseEntity
                 .status(errorCode.getStatus())
+                .body(response);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    protected ResponseEntity<ErrorResponseDto> handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
+        ErrorCode errorCode = ErrorCode.INVALID_INPUT_VALUE;
+        ErrorResponseDto response = ErrorResponseDto.error(errorCode);
+        return ResponseEntity.status(errorCode.getStatus())
+                .body(response);
+    }
+
+    @ExceptionHandler(BindException.class)
+    protected ResponseEntity<ErrorResponseDto> handleBindException(BindException e) {
+        ErrorCode errorCode = ErrorCode.REQUEST_ERROR;
+        ErrorResponseDto response = ErrorResponseDto.error(errorCode);
+        return ResponseEntity.status(errorCode.getStatus())
                 .body(response);
     }
 
--- src/main/java/com/boardwe/boardwe/exception/ErrorCode.java
+++ src/main/java/com/boardwe/boardwe/exception/ErrorCode.java
@@ -3,6 +3,8 @@ package com.boardwe.boardwe.exception;
 public enum ErrorCode {
     // -------- 4xx --------
     REQUEST_ERROR(400,"잘못된 요청입니다."),
+    INVALID_INPUT_VALUE(400, "입력값이 유효하지 않습니다."),
+
     ENTITY_NOT_FOUND(400, "해당 리소스가 존재하지 않습니다."),
     BOARD_NOT_FOUND(400, "해당 보드가 존재하지 않습니다."),
     IMAGE_INFO_NOT_FOUND(400, "해당 이미지 정보가 존재하지 않습니다."),
