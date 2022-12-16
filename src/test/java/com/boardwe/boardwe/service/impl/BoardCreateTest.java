c28ec6423fc0606651ff78e20302884682226ff3
yjkim <31876648+jcdad3000@users.noreply.github.com>
2022-12-16 23:02:09 +0900

    [feat] board create Test 생성
    - board create Test 생성
    - 테마 아이디 받을때 생성 로직 검증
    - 배경 이미지 받을때 생성 로직 검증
    - 배경 컬러 받을때 생성 로직 검증
    - 잘못된 보드 테마 ID를 받았을때 생성 에러 검증
    - writing, open time설정이 잘못되었을때 보드 생성 에러 테스트

---
+++ src/test/java/com/boardwe/boardwe/service/impl/BoardCreateTest.java
@@ -0,0 +1,251 @@
+package com.boardwe.boardwe.service.impl;
+
+import com.boardwe.boardwe.dto.req.BoardCreateRequestDto;
+import com.boardwe.boardwe.dto.req.inner.MemoThemesCreateRequestDto;
+import com.boardwe.boardwe.exception.custom.entity.BoardThemeNotFoundException;
+import com.boardwe.boardwe.exception.custom.other.InvalidDateValueException;
+import com.boardwe.boardwe.service.BoardService;
+import com.boardwe.boardwe.type.OpenType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@SpringBootTest
+@Transactional
+public class BoardCreateTest {
+
+
+    @Autowired
+    private BoardService boardService;
+
+
+
+    @Test
+    @DisplayName("보드 테마 ID를 주었을때 보드 생성 테스트")
+    void createBoardWithBoardThemeId(){
+        String name = "test";
+        String description = "test";
+        List<String> tags = new ArrayList<>();
+        tags.add("크리스마스");
+        tags.add("신년");
+        LocalDateTime writingStartTime = LocalDateTime.now().plusDays(1);
+        LocalDateTime writingEndTime = LocalDateTime.now().plusDays(2);
+        LocalDateTime openStartTime = LocalDateTime.now().plusDays(3);
+        LocalDateTime openEndTime = LocalDateTime.now().plusDays(4);
+        String password = "1234";
+        OpenType openType = OpenType.PUBLIC;
+        Long boardThemeId = 4L;
+
+        BoardCreateRequestDto newBoard = BoardCreateRequestDto.builder()
+                .boardName(name)
+                .boardDescription(description)
+                .tags(tags)
+                .writingStartTime(writingStartTime)
+                .writingEndTime(writingEndTime)
+                .openStartTime(openStartTime)
+                .openEndTime(openEndTime)
+                .password(password)
+                .openType(openType)
+                .boardThemeId(boardThemeId)
+                .build();
+
+        String boardLink = boardService.createBoard(newBoard).getBoardLink();
+
+        Assertions.assertNotNull(boardLink);
+    }
+
+    @Test
+    @DisplayName("보드 이미지를 주었을때 보드 생성 테스트")
+    void createBoardWithBoardImage(){
+        String name = "test";
+        String description = "test";
+        List<String> tags = new ArrayList<>();
+        tags.add("크리스마스");
+        tags.add("신년");
+        LocalDateTime writingStartTime = LocalDateTime.now().plusDays(1);
+        LocalDateTime writingEndTime = LocalDateTime.now().plusDays(2);
+        LocalDateTime openStartTime = LocalDateTime.now().plusDays(3);
+        LocalDateTime openEndTime = LocalDateTime.now().plusDays(4);
+        String password = "1234";
+        OpenType openType = OpenType.PUBLIC;
+
+        String imageBase64 = makeImageToBase64();
+
+        String boardFont = "Batang";
+
+        List<String> backgrounds = new ArrayList<>();
+        backgrounds.add("#efefef");
+        backgrounds.add(imageBase64);
+
+        List<String> textColors = new ArrayList<>();
+        textColors.add("#000000");
+        textColors.add("#FFFFFF");
+
+
+        MemoThemesCreateRequestDto memoTheme = MemoThemesCreateRequestDto.builder()
+                .backgrounds(backgrounds)
+                .textColors(textColors)
+                .build();
+
+        BoardCreateRequestDto newBoard = BoardCreateRequestDto.builder()
+                .boardName(name)
+                .boardDescription(description)
+                .tags(tags)
+                .writingStartTime(writingStartTime)
+                .writingEndTime(writingEndTime)
+                .openStartTime(openStartTime)
+                .openEndTime(openEndTime)
+                .password(password)
+                .openType(openType)
+                .boardBackground(imageBase64)
+                .boardFont(boardFont)
+                .memoThemes(memoTheme)
+                .build();
+
+        String boardLink = boardService.createBoard(newBoard).getBoardLink();
+
+        Assertions.assertNotNull(boardLink);
+    }
+
+    private static String makeImageToBase64() {
+        String imageBase64;
+        File file = new File("./files/imageBase64Test.txt");
+        try{
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            imageBase64 = br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return imageBase64;
+    }
+
+    @Test
+    @DisplayName("보드 컬러를 주었을때 보드 생성 테스트")
+    void createBoardWithBoardColor(){
+        String name = "test";
+        String description = "test";
+        List<String> tags = new ArrayList<>();
+        tags.add("크리스마스");
+        tags.add("신년");
+        LocalDateTime writingStartTime = LocalDateTime.now().plusDays(1);
+        LocalDateTime writingEndTime = LocalDateTime.now().plusDays(2);
+        LocalDateTime openStartTime = LocalDateTime.now().plusDays(3);
+        LocalDateTime openEndTime = LocalDateTime.now().plusDays(4);
+        String password = "1234";
+        OpenType openType = OpenType.PUBLIC;
+
+        String imageBase64 = makeImageToBase64();
+
+        String boardFont = "Batang";
+
+        List<String> backgrounds = new ArrayList<>();
+        backgrounds.add("#efefef");
+        backgrounds.add(imageBase64);
+
+        List<String> textColors = new ArrayList<>();
+        textColors.add("#000000");
+        textColors.add("#FFFFFF");
+
+
+        MemoThemesCreateRequestDto memoTheme = MemoThemesCreateRequestDto.builder()
+                .backgrounds(backgrounds)
+                .textColors(textColors)
+                .build();
+
+        BoardCreateRequestDto newBoard = BoardCreateRequestDto.builder()
+                .boardName(name)
+                .boardDescription(description)
+                .tags(tags)
+                .writingStartTime(writingStartTime)
+                .writingEndTime(writingEndTime)
+                .openStartTime(openStartTime)
+                .openEndTime(openEndTime)
+                .password(password)
+                .openType(openType)
+                .boardBackground("#FFFFFF")
+                .boardFont(boardFont)
+                .memoThemes(memoTheme)
+                .build();
+
+        String boardLink = boardService.createBoard(newBoard).getBoardLink();
+
+        Assertions.assertNotNull(boardLink);
+    }
+    @Test
+    @DisplayName("잘못된 보드 테마 ID를 주었을때 보드 생성 에러 테스트")
+    void createBoardWithWrongBoardThemeId(){
+        String name = "test";
+        String description = "test";
+        List<String> tags = new ArrayList<>();
+        tags.add("크리스마스");
+        tags.add("신년");
+        LocalDateTime writingStartTime = LocalDateTime.now().plusDays(1);
+        LocalDateTime writingEndTime = LocalDateTime.now().plusDays(2);
+        LocalDateTime openStartTime = LocalDateTime.now().plusDays(3);
+        LocalDateTime openEndTime = LocalDateTime.now().plusDays(4);
+        String password = "1234";
+        OpenType openType = OpenType.PUBLIC;
+        Long boardThemeId = 1L;
+
+        BoardCreateRequestDto newBoard = BoardCreateRequestDto.builder()
+                .boardName(name)
+                .boardDescription(description)
+                .tags(tags)
+                .writingStartTime(writingStartTime)
+                .writingEndTime(writingEndTime)
+                .openStartTime(openStartTime)
+                .openEndTime(openEndTime)
+                .password(password)
+                .openType(openType)
+                .boardThemeId(boardThemeId)
+                .build();
+
+        Assertions.assertThrows(BoardThemeNotFoundException.class,()-> boardService.createBoard(newBoard).getBoardLink());
+    }
+    @Test
+    @DisplayName("보드 생성 시간을 잘못 주었을 때 보드 생성 에러 테스트")
+    void createBoardWithTimeError(){
+        String name = "test";
+        String description = "test";
+        List<String> tags = new ArrayList<>();
+        tags.add("크리스마스");
+        tags.add("신년");
+        LocalDateTime writingStartTime = LocalDateTime.now().plusDays(1);
+        LocalDateTime writingEndTime = LocalDateTime.now().plusDays(5);
+        LocalDateTime openStartTime = LocalDateTime.now().plusDays(3);
+        LocalDateTime openEndTime = LocalDateTime.now().plusDays(4);
+        String password = "1234";
+        OpenType openType = OpenType.PUBLIC;
+        Long boardThemeId = 4L;
+
+        BoardCreateRequestDto newBoard = BoardCreateRequestDto.builder()
+                .boardName(name)
+                .boardDescription(description)
+                .tags(tags)
+                .writingStartTime(writingStartTime)
+                .writingEndTime(writingEndTime)
+                .openStartTime(openStartTime)
+                .openEndTime(openEndTime)
+                .password(password)
+                .openType(openType)
+                .boardThemeId(boardThemeId)
+                .build();
+
+
+        Assertions.assertThrows(InvalidDateValueException.class,()->{
+            boardService.createBoard(newBoard).getBoardLink();
+        });
+    }
+
+
+}
