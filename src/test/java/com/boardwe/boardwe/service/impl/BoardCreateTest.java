9d024eb617e11eafc88df3d70809697765ff3b7e
yjkim <31876648+jcdad3000@users.noreply.github.com>
2022-12-21 22:33:35 +0900

    [bugfix] board create Unit Test
    -Mockito를 활용한 단위테스트로 변경

--- src/test/java/com/boardwe/boardwe/service/impl/BoardCreateTest.java
+++ src/test/java/com/boardwe/boardwe/service/impl/BoardCreateTest.java
@@ -2,32 +2,76 @@ package com.boardwe.boardwe.service.impl;
 
 import com.boardwe.boardwe.dto.req.BoardCreateRequestDto;
 import com.boardwe.boardwe.dto.req.inner.MemoThemesCreateRequestDto;
+import com.boardwe.boardwe.entity.Board;
+import com.boardwe.boardwe.entity.BoardTheme;
+import com.boardwe.boardwe.entity.ImageInfo;
+import com.boardwe.boardwe.entity.ThemeCategory;
 import com.boardwe.boardwe.exception.custom.entity.BoardThemeNotFoundException;
 import com.boardwe.boardwe.exception.custom.other.InvalidDateValueException;
+import com.boardwe.boardwe.repository.*;
 import com.boardwe.boardwe.service.BoardService;
 import com.boardwe.boardwe.type.OpenType;
+import com.boardwe.boardwe.util.FileUtil;
+import com.boardwe.boardwe.util.ThemeUtil;
+import com.boardwe.boardwe.util.impl.BoardInfoUtilImpl;
+import com.boardwe.boardwe.vo.ImageInfoVo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.persistence.MapKeyColumn;
 import javax.transaction.Transactional;
 import java.io.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 
-@SpringBootTest
-@Transactional
+@ExtendWith(MockitoExtension.class)
 public class BoardCreateTest {
 
+    @Mock
+    Board board;
 
-    @Autowired
-    private BoardService boardService;
+    @Mock
+    BoardThemeRepository boardThemeRepository;
 
+    @Mock
+    BoardRepository boardRepository;
 
+    @Mock
+    BoardCreateRequestDto boardCreateRequestDto;
+
+    @Mock
+    TagRepository tagRepository;
+
+    @Mock
+    ThemeUtil themeUtil;
+    @Mock
+    ThemeCategory themeCategory;
+    @Mock
+    FileUtil fileUtil;
+    @Mock
+    ImageInfoRepository imageInfoRepository;
+
+    @Mock
+    BoardInfoUtilImpl boardInfoUtil;
+    @Mock
+    MemoThemeRepository memoThemeRepository;
+    @InjectMocks
+    private BoardServiceImpl boardService;
 
     @Test
     @DisplayName("보드 테마 ID를 주었을때 보드 생성 테스트")
@@ -57,6 +101,13 @@ public class BoardCreateTest {
                 .openType(openType)
                 .boardThemeId(boardThemeId)
                 .build();
+
+        BoardTheme boardTheme = mock(BoardTheme.class);
+        Board board = mock(Board.class);
+        when(boardThemeRepository.findById(4L)).thenReturn(Optional.of(boardTheme));
+        when(boardRepository.save(any())).thenReturn(board);
+        when(board.getCode()).thenReturn("test");
+        when(boardInfoUtil.getBoardLink("test")).thenReturn("/board/test");
 
         String boardLink = boardService.createBoard(newBoard).getBoardLink();
 
@@ -110,6 +161,22 @@ public class BoardCreateTest {
                 .boardFont(boardFont)
                 .memoThemes(memoTheme)
                 .build();
+
+        BoardTheme boardTheme = mock(BoardTheme.class);
+        Board board = mock(Board.class);
+        ImageInfoVo imageInfoVo = mock(ImageInfoVo.class);
+        ImageInfo imageInfo = mock(ImageInfo.class);
+        ThemeCategory themeCategory = mock(ThemeCategory.class);
+
+
+        when(themeUtil.getUserThemeCategory()).thenReturn(themeCategory);
+        when(themeUtil.getUserThemeName()).thenReturn("TEMP");
+        when(fileUtil.saveImage(imageBase64,"imageTempName.jpg")).thenReturn(imageInfoVo);
+        when(imageInfoRepository.save(any())).thenReturn(imageInfo);
+        when(boardThemeRepository.save(any())).thenReturn(boardTheme);
+        when(boardRepository.save(any())).thenReturn(board);
+        when(board.getCode()).thenReturn("test");
+        when(boardInfoUtil.getBoardLink("test")).thenReturn("/board/test");
 
         String boardLink = boardService.createBoard(newBoard).getBoardLink();
 
@@ -177,6 +244,21 @@ public class BoardCreateTest {
                 .memoThemes(memoTheme)
                 .build();
 
+        BoardTheme boardTheme = mock(BoardTheme.class);
+        Board board = mock(Board.class);
+        ImageInfoVo imageInfoVo = mock(ImageInfoVo.class);
+        ImageInfo imageInfo = mock(ImageInfo.class);
+        ThemeCategory themeCategory = mock(ThemeCategory.class);
+
+        when(themeUtil.getUserThemeCategory()).thenReturn(themeCategory);
+        when(themeUtil.getUserThemeName()).thenReturn("TEMP");
+        when(fileUtil.saveImage(imageBase64,"imageTempName.jpg")).thenReturn(imageInfoVo);
+        when(imageInfoRepository.save(any())).thenReturn(imageInfo);
+        when(boardThemeRepository.save(any())).thenReturn(boardTheme);
+        when(boardRepository.save(any())).thenReturn(board);
+        when(board.getCode()).thenReturn("test");
+        when(boardInfoUtil.getBoardLink("test")).thenReturn("/board/test");
+
         String boardLink = boardService.createBoard(newBoard).getBoardLink();
 
         Assertions.assertNotNull(boardLink);
@@ -211,6 +293,7 @@ public class BoardCreateTest {
                 .build();
 
         Assertions.assertThrows(BoardThemeNotFoundException.class,()-> boardService.createBoard(newBoard).getBoardLink());
+
     }
     @Test
     @DisplayName("보드 생성 시간을 잘못 주었을 때 보드 생성 에러 테스트")
@@ -240,6 +323,11 @@ public class BoardCreateTest {
                 .openType(openType)
                 .boardThemeId(boardThemeId)
                 .build();
+
+        BoardTheme boardTheme = mock(BoardTheme.class);
+        Board board = mock(Board.class);
+        when(boardThemeRepository.findById(4L)).thenReturn(Optional.of(boardTheme));
+
 
 
         Assertions.assertThrows(InvalidDateValueException.class,()->{
