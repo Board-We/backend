4598a2a35acb6b4c88cf4574cc3a5eb19b519ff7
jjungyeun <wjyddd@naver.com>
2023-02-02 01:27:40 +0900

    [chore] health check 설정을 config로 이동
    -  LogAspect에 추가되지 않도록 config 패키지로 이동

--- src/main/java/com/boardwe/boardwe/config/HealthCheck.java
+++ src/main/java/com/boardwe/boardwe/config/HealthCheck.java
@@ -1,11 +1,11 @@
-package com.boardwe.boardwe.controller;
+package com.boardwe.boardwe.config;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HealthCheckController {
+public class HealthCheck {
 
     // health check API for AWS Load balancer
     @GetMapping("/health")
