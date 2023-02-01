21d5230aac989c8901d5a363c74e39a18c66b091
jjungyeun <wjyddd@naver.com>
2023-02-02 00:57:58 +0900

    [chore] AWS 로드밸런서 등록을 위한 health check API 추가

---
+++ src/main/java/com/boardwe/boardwe/controller/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.boardwe.boardwe.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    // health check API for AWS Load balancer
+    @GetMapping("/health")
+    public ResponseEntity<Void> healthCheck() {
+        return ResponseEntity.ok().build();
+    }
+
+}
