a9923e5dd1bfe6151327745dfe0802beb36cd1ba
jjungyeun <wjyddd@naver.com>
2023-02-07 23:27:11 +0900

    [fix] @ManyToOne 연결관계에 Lazy Loading 적용
    - 엔티티 조회 시 Eager Loading이 되지 않도록 설정 추가

--- src/main/java/com/boardwe/boardwe/entity/Tag.java
+++ src/main/java/com/boardwe/boardwe/entity/Tag.java
@@ -18,7 +18,7 @@ public class Tag {
     @Column(name = "tag_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_id")
     @NotNull
     private Board board;
